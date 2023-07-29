2ddbd7fd2f279949fb46a63d11d037038bd481f5
Yaroslav <yaroslavkyzyk@gmail.com>
2023-07-29 20:01:32 +0300

    Demo > Added Several Packages & Extensions + Created Demo With Graphics

---
+++ Assets/Game/Scripts/Extensions/MonoExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Events;
+using System.Collections;
+using UnityEngine;
+
+
+namespace Game.Extensions
+{
+    public static class MonoExtensions
+    {
+        private static readonly Dictionary<float, WaitForSeconds> WaitForSecondsDictionary = new();
+
+        private static readonly WaitForFixedUpdate WaitForEndOfFixedUpdate = new();
+        private static readonly WaitForEndOfFrame WaitForEndOfFrame = new();
+
+
+        public static Coroutine Wait(this MonoBehaviour mono, float delay, UnityAction action)
+            => mono.StartCoroutine(ExecuteAction(delay, action));
+
+        public static WaitForEndOfFrame GetWaitForEndOfFrame()
+            => WaitForEndOfFrame;
+
+        public static WaitForFixedUpdate GetWaitForFixedUpdate()
+            => WaitForEndOfFixedUpdate;
+
+        public static WaitForSeconds GetWait(float time)
+        {
+            if (!WaitForSecondsDictionary.TryGetValue(time, out var wait))
+            {
+                wait = new WaitForSeconds(time);
+                
+                WaitForSecondsDictionary[time] = wait;
+            }
+
+            return wait;
+        }
+
+        private static IEnumerator ExecuteAction(float delay, UnityAction action)
+        {
+            yield return new WaitForSecondsRealtime(delay);
+
+            action?.Invoke();
+        }
+
+        public static bool IsNull(this MonoBehaviour obj) =>
+            (object)obj == null;
+
+        public static bool IsNotNull(this MonoBehaviour obj) =>
+            !IsNull(obj);
+    }
+}
--- Assets/Game/Scripts/Extensions/WorldPoint.cs
+++ Assets/Game/Scripts/Extensions/WorldPoint.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using UnityEditor;
 using UnityEngine;
 
 
@@ -19,24 +18,6 @@ namespace Game.Extensions
                 throw new ArgumentException("World point have null transform");
 
             return transform.position;
-        }
-    }
-
-    [CustomPropertyDrawer(typeof(WorldPoint))]
-    public class WorldPointPropertyDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            EditorGUI.BeginProperty(position, label, property);
-
-            var indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            var transformSerializedProperty = property.FindPropertyRelative("_point");
-            EditorGUI.ObjectField(position, transformSerializedProperty, typeof(Transform), label);
-
-            EditorGUI.indentLevel = indent;
-            EditorGUI.EndProperty();
         }
     }
 }
